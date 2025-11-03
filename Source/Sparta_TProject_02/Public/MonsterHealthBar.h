e668888b50963e8b6bf35727d61b92c34e58e2d2
Veritus <tls9603@gmail.com>
2025-11-03 16:53:41 +0900

    Create MonsterHealthBar.h

---
+++ Source/Sparta_TProject_02/Public/MonsterHealthBar.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MonsterHealthBar.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPARTA_TPROJECT_02_API UMonsterHealthBar : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintImplementableEvent, Category = "Health")
+	void UpdateHealthBar(float HealthRatio);
+};
