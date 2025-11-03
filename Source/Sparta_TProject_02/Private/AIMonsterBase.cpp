592599510f8d5195e3ac21f66b21142401285ce0
Veritus <tls9603@gmail.com>
2025-11-03 16:54:09 +0900

    Update AIMonsterBase.cpp

--- Source/Sparta_TProject_02/Private/AIMonsterBase.cpp
+++ Source/Sparta_TProject_02/Private/AIMonsterBase.cpp
@@ -8,6 +8,10 @@
 #include "Sparta_TProject_02/Sparta_TProject_02Character.h"
 #include "AIController.h"
 #include "BrainComponent.h"
+#include "Components/WidgetComponent.h"
+#include "Blueprint/UserWidget.h"
+#include "UObject/UObjectGlobals.h"
+#include "MonsterHealthBar.h"
 
 AAIMonsterBase::AAIMonsterBase()
 {
@@ -25,17 +29,36 @@ AAIMonsterBase::AAIMonsterBase()
     GetCharacterMovement()->bOrientRotationToMovement = false;
     GetCharacterMovement()->bUseControllerDesiredRotation = true;
     GetCharacterMovement()->RotationRate = FRotator(0.0f, 360.0f, 0.0f);
+
+    HealthBarWidgetComponent = CreateDefaultSubobject<UWidgetComponent>(TEXT("HealthBarWidget"));
+    HealthBarWidgetComponent->SetupAttachment(GetMesh());
+    HealthBarWidgetComponent->SetWidgetSpace(EWidgetSpace::Screen);
+    HealthBarWidgetComponent->SetDrawAtDesiredSize(true);
+    HealthBarWidgetComponent->SetRelativeLocation(FVector(0.f, 0.f, 200.f));
 }
 
 void AAIMonsterBase::BeginPlay()
 {
     Super::BeginPlay();
     CurrentHealth = MaxHealth; // 게임 시작 시 체력 초기화
+
+    if (HealthBarWidgetComponent)
+    {
+        HealthBarWidgetComponent->SetVisibility(false);
+    }
 }
 
 void AAIMonsterBase::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
+}
+
+void AAIMonsterBase::HideHealthBar()
+{
+    if (HealthBarWidgetComponent)
+    {
+        HealthBarWidgetComponent->SetVisibility(false);
+    }
 }
 
 float AAIMonsterBase::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
@@ -49,6 +72,21 @@ float AAIMonsterBase::TakeDamage(float DamageAmount, FDamageEvent const& DamageE
     if (ActualDamage > 0.0f)
     {
         CurrentHealth -= ActualDamage;
+        
+        if (HealthBarWidgetComponent)
+        {
+            HealthBarWidgetComponent->SetVisibility(true);
+
+            UMonsterHealthBar* HealthBar = Cast<UMonsterHealthBar>(HealthBarWidgetComponent->GetUserWidgetObject());
+
+            if (HealthBar)
+            {
+                float HealthRatio = FMath::Clamp(CurrentHealth / MaxHealth, 0.0f, 1.0f);
+                HealthBar->UpdateHealthBar(HealthRatio);
+            }
+            GetWorldTimerManager().SetTimer(HealthBarTimerHandle, this, &AAIMonsterBase::HideHealthBar, 3.0f, false);
+        }
+
         UE_LOG(LogTemp, Warning, TEXT("%s's Health: %f"), *GetName(), CurrentHealth);
 
         if (CurrentHealth <= 0.0f)
@@ -119,6 +157,11 @@ void AAIMonsterBase::Die()
 
     bIsDead = true;
     UE_LOG(LogTemp, Warning, TEXT("%s has died."), *GetName());
+
+    if (HealthBarWidgetComponent)
+    {
+        HealthBarWidgetComponent->SetVisibility(false);
+    }
 
     AAIController* AIController = Cast<AAIController>(GetController());
     if (AIController && AIController->GetBrainComponent())
