62f64849f61f0ec1280f0a37e741dcfee5fb73cf
geumseong <79560547+geumseong@users.noreply.github.com>
2025-11-06 09:25:51 +0900

    Feat: 상점UI 내의 인벤토리 반영

--- Source/Sparta_TProject_02/Public/ShopUserWidget.h
+++ Source/Sparta_TProject_02/Public/ShopUserWidget.h
@@ -47,6 +47,9 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Widgets")
 	TSubclassOf<UShopItemEntryWidget> ItemEntryWidgetClass;
 
+	UPROPERTY(meta = (BindWidget))
+	class UPanelWidget* InventoryContainer;
+
 private:
 	UFUNCTION()
 	void OnCloseButtonClicked();
