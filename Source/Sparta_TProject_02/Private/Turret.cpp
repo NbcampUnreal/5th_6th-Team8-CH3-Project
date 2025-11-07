9df16c742a3eae374a34fd299a565875cf8bceff
soloplayer-tech <nos36650@gmail.com>
2025-11-07 14:12:05 +0900

    임시
    임시

--- Source/Sparta_TProject_02/Private/Turret.cpp
+++ Source/Sparta_TProject_02/Private/Turret.cpp
@@ -35,11 +35,14 @@ void ATurret::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// 터렛의 현재 월드 회전으로 초기화. 조준 시 이 값을 기준으로 보간합니다.
+	if (SpawnSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(GetWorld(), SpawnSound, GetActorLocation());
+	}
+
 	CurrentTurretRotation = GetActorRotation();
 
 	GetWorldTimerManager().SetTimer(ScanTimerHandle, this, &ATurret::ScanForTargets, 0.3f, true);
-	// 타겟이 없으면 멈춘 상태로 시작
 	GetWorldTimerManager().SetTimer(FireTimerHandle, this, &ATurret::Fire, FireInterval, true);
 	GetWorldTimerManager().PauseTimer(FireTimerHandle);
 
@@ -53,21 +56,18 @@ void ATurret::Tick(float DeltaTime)
 	if (bHasTarget && CurrentTarget)
 	{
 		AimAtTarget(DeltaTime);
-		// 타겟이 있을 때만 발사 타이머가 돌아가도록 관리
 		if (GetWorldTimerManager().IsTimerPaused(FireTimerHandle))
 			GetWorldTimerManager().UnPauseTimer(FireTimerHandle);
 	}
 	else
 	{
 		RotateIdle(DeltaTime);
-		// 타겟을 잃으면 발사 타이머 정지
 		if (!GetWorldTimerManager().IsTimerPaused(FireTimerHandle))
 			GetWorldTimerManager().PauseTimer(FireTimerHandle);
 	}
 
 	if (UTurretAnimInstance* AnimInst = Cast<UTurretAnimInstance>(TurretMesh->GetAnimInstance()))
 	{
-		// 서서히 BarrelOffset 복귀 (리코일 되돌리기)
 		AnimInst->BarrelOffset = FMath::FInterpTo(AnimInst->BarrelOffset, 0.f, DeltaTime, 15.f);
 	}
 }
@@ -78,18 +78,15 @@ void ATurret::RotateIdle(float DeltaTime)
 
 	if (UTurretAnimInstance* AnimInst = Cast<UTurretAnimInstance>(TurretMesh->GetAnimInstance()))
 	{
-		// 애님 인스턴스의 HeadRotation을 기준으로 회전 계산
 		float CurrentYaw = AnimInst->HeadRotation.Yaw;
 		float NewYaw = CurrentYaw + Direction * RotationSpeed * DeltaTime;
 
-		// -60도 ~ 60도 제한 (Head Bone의 로컬 회전 기준)
 		if (FMath::Abs(NewYaw) >= 60.f)
 		{
 			Direction *= -1.f;
 			NewYaw = FMath::Clamp(NewYaw, -60.f, 60.f);
 		}
 
-		// Yaw만 회전 적용
 		AnimInst->HeadRotation.Yaw = NewYaw;
 		AnimInst->HeadRotation.Pitch = 0.f;
 	}
@@ -115,7 +112,6 @@ void ATurret::ScanForTargets()
 		}
 	}
 
-	// 상태 업데이트
 	bHasTarget = (ClosestTarget != nullptr);
 	CurrentTarget = ClosestTarget;
 }
@@ -129,14 +125,12 @@ void ATurret::AimAtTarget(float DeltaTime)
 
 	FRotator TurretWorldRot = GetActorRotation();
 
-	// 부드러운 회전을 위해 CurrentTurretRotation(월드 회전)을 TargetWorldRot으로 보간
 	CurrentTurretRotation = FMath::RInterpTo(CurrentTurretRotation, TargetWorldRot, DeltaTime, 5.f);
 
 	FRotator RelativeRot = UKismetMathLibrary::NormalizedDeltaRotator(CurrentTurretRotation, TurretWorldRot); 
 
 	if (UTurretAnimInstance* AnimInst = Cast<UTurretAnimInstance>(TurretMesh->GetAnimInstance()))
 	{
-		// Yaw와 Pitch만 사용
 		AnimInst->HeadRotation.Yaw = RelativeRot.Yaw;
 		AnimInst->HeadRotation.Pitch = RelativeRot.Pitch;
 		AnimInst->HeadRotation.Roll = 0.f;
@@ -156,10 +150,9 @@ void ATurret::Fire()
 	if (FireSound)
 		UGameplayStatics::PlaySoundAtLocation(GetWorld(), FireSound, MuzzleLoc);
 
-	// 리코일 효과: 애님 인스턴스에 전달
 	if (UTurretAnimInstance* AnimInst = Cast<UTurretAnimInstance>(TurretMesh->GetAnimInstance()))
 	{
-		AnimInst->BarrelOffset = -5.f; // 포신 뒤로 살짝 당김
+		AnimInst->BarrelOffset = -5.f; 
 	}
 
 	FActorSpawnParameters Params;
@@ -177,12 +170,22 @@ void ATurret::TakeTurretDamage(float Amount)
 	Health -= Amount;
 	if (Health <= 0.f)
 	{
-		Destroy();
+		DestroySelf();
 	}
 }
 
 void ATurret::DestroySelf()
 {
 	GetWorldTimerManager().ClearAllTimersForObject(this);
+
+	if (DestroyEffect)
+	{
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), DestroyEffect, GetActorLocation(), GetActorRotation());
+	}
+	if (DestroySound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(GetWorld(), DestroySound, GetActorLocation());
+	}
+
 	Destroy();
 }
